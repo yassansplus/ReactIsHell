f9a28f82857da6f28dd2326c0ded67e52f6cd135
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2021-02-11 12:50:50 +0100

    Debut du Component: shouldUpdate et render, manque le display, à faire quand on aura les autres components

--- src/Component.js
+++ src/Component.js
@@ -1,8 +1,26 @@
+import checkType from './TypeChecker.js'
 
 export default class Component {
 
     propertiesConfiguration = null
     template = ""
+
+
+    // Display is asynchronous and returns a Promise, because it needs to wait for render(...) to be completed
+    display(newProps) {
+        if(newProps) {
+            if(!checkType(newProps, this.propertiesConfiguration)){
+                throw new Error("Wrong configuration");
+            }
+        }
+        if(this.shouldUpdate(newProps)) {
+            return this.render().then(() => {
+                return this.template;
+            })
+        } else {
+            return Promise.resolve(this.template);
+        }
+    }
 
     shouldUpdate(newProps) {
         if(newProps !== this.props) {
@@ -14,7 +32,7 @@ export default class Component {
     }
 
     // Render is asynchronous because we might want to load some datas from an external source before compiling
-    // the template.
+    // the template.vasy c
     render() {
         this.template = this.template.interpolate(this.props);
         return Promise.resolve(null)
